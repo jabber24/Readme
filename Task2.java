9bb25c6e93fc707e3b99bbf7f35dac7c1e523a81
jabber24 <151489288+jabber24@users.noreply.github.com>
2026-06-20 03:12:26 +0000

    task

---
+++ Task2.java
@@ -0,0 +1,28 @@
+public class Task2. {
+    public static void main(String[] args) {
+
+        
+
+        char H = 'H';
+
+        byte three = 3;
+
+        int one = 1;
+
+        short zero = 0;
+
+        float twoPointZero = 2.0f;
+
+        boolean truth = true;
+
+
+
+        String output = "" + H + three + one + one + zero + " w" + zero + "r" + one + "d " + twoPointZero + " " + truth;
+
+       
+
+        System.out.println(output);
+
+    }
+
+}
